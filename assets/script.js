dcb77ce164928e49e1a5c6ae540eaef3f608d5c9
Jacob Rosenberg <55591951+JMRosenberg12@users.noreply.github.com>
2019-11-07 08:37:48 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,27 @@
-//generate password
+
 function generate(){
-//set password length/complexity
+
 let complexity = document.getElementById("slider").value;
 
-//passible password values
 let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_="
 
 let password = "";
 
-//create for loop to choose password characters
+
 for(var i= 0; i <= complexity; i++){
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
 }
 
-//add password to textbox/display area
+
 document.getElementById("display").value = password;
 
-//add password to previously generate passwords section
+
 document.getElementById("lastNums").innerHTML +=password + "<br /r>";
 }
 
-//set default length of 30
+
 document.getElementById("length").innerHTML = "length : 30";
 
-//function to set length based on slider position
 document.getElementById("slider").oninput = function() {
     if (document.getElementById("slider").value > 0) {
         document.getElementById("length").innerHTML = "length: "+ document.getElementById("slider").value;
@@ -32,7 +30,7 @@ document.getElementById("slider").oninput = function() {
         document.getElementById("length").innerHTML = "length: 1"
     }
 }
-//function to copy password to clipboard
+
 function copyPassword(){
    document.getElementById("display").select();
    document.execCommand("copy");
