02e6d48d6fd13bb763636358ee91baad5bd0ee01
Jacob Rosenberg <55591951+JMRosenberg12@users.noreply.github.com>
2019-11-08 08:12:24 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,21 @@ function copyPassword(){
    document.execCommand("copy");
    alert ("Password copied to clipboard!");
 }
-function lowercase(){
+function lowercase() {
     var hasUserSelectedLowerCase = confirm ("Do you want your pass to contain lower case letters?");
     
 }
+function uppercase() {
+    var hasUserSelectedUpperCase = confirm ("Do you want your pass to contain lower cass letters?");
 
+}
+function special() {
+    var hasUserSelectedSpecial = Confirm ("Do you want your pass to contain special keys?")
+}
+
+function special() {
+    var hasUserSelectedNumbers = confirm ("Do you want your pass to contain numbers?")
+}
 
 
 function generate(){
@@ -24,11 +34,18 @@ var complexity = prompt ("How many letters you want in your password?");
 
 
 for(var i= 0; i <= complexity; i++){
-    
-    
-    if (hasUserSelectedLowerCase){
+    if(hasUserSelectedUpperCase){
+        password = password + value2.charAt(Math.floor(Math.random() * Math.floor(value2.length - 1)));  
+    }
+    if(hasUserSelectedLowerCase){
         password = password + value3.charAt(Math.floor(Math.random() * Math.floor(value3.length - 1)));
 
+    }
+    if(hasUserSelectedNumbers){
+        password = password + value4.charAt(Math.floor(Math.random() * Math.floor(value4.length - 1)));
+    }
+    if(hasUserSelectedSpecial){
+        password = Password + value1.charAt(Math.floor(Math.random() * Math.floor(value1.length - 1)));
     }
     
   
