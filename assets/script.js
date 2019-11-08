4564ed230b900c908aaebf83f30642ecbbc60607
Jacob Rosenberg <55591951+JMRosenberg12@users.noreply.github.com>
2019-11-08 08:08:18 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,7 @@ function lowercase() {
     var hasUserSelectedLowerCase = confirm ("Do you want your pass to contain lower case letters?");
     
 }
-function uppercase() {
-    var hasUserSelectedUpperCase = confirm ("Do you want your pass to contain lower cass letters?");
 
-}
-function special () {
-    var hasUserSelectedSpecial = confirm ("Do you want your pass to contain special keys?")
-}
-
-function special () {
-    var hasUserSelectedNumbers = confirm ("Do you want your pass to contain numbers?")
-}
 
 
 function generate(){
@@ -34,18 +24,11 @@ var complexity = prompt ("How many letters you want in your password?");
 
 
 for(var i= 0; i <= complexity; i++){
-    if (hasUserSelectedUpperCase){
-        password = password + value2.charAt(Math.floor(Math.random() * Math.floor(value2.length - 1)));  
-    }
+    
+    
     if (hasUserSelectedLowerCase){
         password = password + value3.charAt(Math.floor(Math.random() * Math.floor(value3.length - 1)));
 
-    }
-    if (hasUserSelectedNumbers){
-        password = password + value4.charAt(Math.floor(Math.random() * Math.floor(value4.length - 1)));
-    }
-    if (hasUserSelectedSpecial){
-        password = Password + value1.charAt(Math.floor(Math.floor() * Math.floor(value1.length - 1)));
     }
     
   
