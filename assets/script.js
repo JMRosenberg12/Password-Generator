a07e5ff99b7bcf3e0e7da020572fc8e0442ceb67
Jacob Rosenberg <55591951+JMRosenberg12@users.noreply.github.com>
2019-11-09 09:48:41 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,77 @@
-function copyPassword(){
-   document.getElementById("display").select();
-   document.execCommand("copy");
-   alert ("Password copied to clipboard!");
+function copyPassword() {
+	document.getElementById("display").select();
+	document.execCommand("copy");
+	alert("Password copied to clipboard!");
 }
-function generate(){
 
 function lowercase() {
-    var hasUserSelectedLowerCase = confirm ("Do you want your pass to contain lower case letters?");
+	var hasUserSelectedLowerCase = confirm("Do you want your pass to contain lower case letters?");
+	return hasUserSelectedLowerCase;
 
-    
 }
+
 function uppercase() {
-    var hasUserSelectedUpperCase = confirm ("Do you want your pass to contain lower cass letters?");
+	var hasUserSelectedUpperCase = confirm("Do you want your pass to contain upper case letters?");
+	return hasUserSelectedUpperCase;
 
-}
-function special() {
-    var hasUserSelectedSpecial = confirm ("Do you want your pass to contain special keys?")
 }
 
 function special() {
-    var hasUserSelectedNumbers = confirm ("Do you want your pass to contain numbers?")
+	var hasUserSelectedSpecial = confirm("Do you want your pass to contain special keys?")
+	return hasUserSelectedSpecial;
 }
 
-let value1 = "!@#$%^&*()";
-let value2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let value3 = "abcdefghijklmnopqrstuvwxyz";
-let value4 = "1234567890";
-
-let password = "";
-var complexity = prompt ("How many letters you want in your password?");
-
-
-for(var i= 0; i <= complexity; i++){
-    if (hasUserSelectedUpperCase){
-        password = password + value2.charAt(Math.floor(Math.random() * Math.floor(value2.length - 1)));  
-    }
-     
-    
-    if (hasUserSelectedLowerCase){
-        password = password + value3.charAt(Math.floor(Math.random() * Math.floor(value3.length - 1)));
-
-    }
-    if (hasUserSelectedNumbers){
-        password = password + value4.charAt(Math.floor(Math.random() * Math.floor(value4.length - 1)));
-    }
-    if (hasUserSelectedSpecial){
-        password = Password + value1.charAt(Math.floor(Math.random() * Math.floor(value1.length - 1)));
-    }
-    
-  
-}
+function numbers() {
+	var hasUserSelectedNumbers = confirm("Do you want your pass to contain numbers?")
+	return hasUserSelectedNumbers;
 }
 
 
-document.getElementById("display").value2 = password;
+function generate() {
+
+		let value1 = "!@#$%^&*()";
+		let value2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let value3 = "abcdefghijklmnopqrstuvwxyz";
+		let value4 = "1234567890";
+
+		let password = "";
+		var complexity = prompt("How many letters you want in your password?");
+
+		var hasUserSelectedUpperCase = uppercase();
+		var hasUserSelectedLowerCase = lowercase();
+		var hasUserSelectedSpecial = special();
+		var hasUserSelectedNumbers = numbers();
 
 
+		for (var i = 0; i <= complexity; i++) {
+			if (hasUserSelectedUpperCase) {
+				password = password + value2.charAt(Math.floor(Math.random() * Math.floor(value2.length - 1)));
+			}
 
-document.getElementById("lastNums").innerHTML = password + "<br /r>";
+
+			if (hasUserSelectedLowerCase) {
+				password = password + value3.charAt(Math.floor(Math.random() * Math.floor(value3.length - 1)));
+
+			}
+			if (hasUserSelectedNumbers) {
+				password = password + value4.charAt(Math.floor(Math.random() * Math.floor(value4.length - 1)));
+			}
+			if (hasUserSelectedSpecial) {
+				password = password + value1.charAt(Math.floor(Math.random() * Math.floor(value1.length - 1)));
+            }
+        }
+   
+
+			document.getElementById("display").value1 = password;
+			document.getElementById("display").value2 = password;
+			document.getElementById("display").value3 = password;
+			document.getElementById("display").value4 = password;
+   
+
+			document.getElementById("lastNums").innerHTML = password + "<br /r>";
 
 
+			document.getElementById("length").innerHTML = "length: 30";
 
-document.getElementById("length").innerHTML = "length: 30";
+    }
 
